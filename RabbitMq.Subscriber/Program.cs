3a37d0ea3c2865282bf330e120dfa7e0f045a148
RAHIME KIR <RAHIME.KIR@lcwaikiki.com>
2023-08-04 12:02:43 +0300

    Add project files.

---
+++ RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMq.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kfsdoqic:jn0RezxxesWFhHedSXKfrxlcieC0PsC_@fish.rmq.cloudamqp.com/kfsdoqic");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+
+            channel.BasicQos(0, 1, false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            var routekey = "*.Error.*";  //Info.#     *.*.Warning   => * tek karaktere karşılık gelir, # birden fazla karakteree
+            channel.QueueBind(queueName, "logs-topic", routekey);
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("Logları dinleniyor...");
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen Mesaj:" + message);
+
+  
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
